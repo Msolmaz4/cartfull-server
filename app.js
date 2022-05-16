505c41411b708c494dba5f786eefcb11e13e061d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-05-16 22:44:51 +0200

    mittelerwe

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express();
 
+
+//connection with db
 require('./db/db')
+
+//parsing the body of the request
+app.use(express.urlencoded({ extended :false}))
+app.use(express.json())
+
+
+// product end point
+app.use('*',(req,res,next)=>{
+    if(!req.originalUrl.includes('/products')){
+        return res.status(404).send('not found')
+        next()
+    }
+    
+})
+
+
+
+app.use('/products',productRouter)
+
+app.use('*',(req,res,next)=>{
+    res.status(404).send('not found server')
+
+}
 
 
 module.exports = app;
--- seeds.js
+++ seeds.js
@@ -6,8 +6,43 @@ const Product = require('./models/products')
 const connectFunction =()=>{
     mongoose.connect('mongodb://localhost:27017/productApp')
     .then(()=>console.log('connect db'))
-    .catch(err =>console.log(errr))
+    .catch((err) =>console.log(errr))
 }
 
 
 connectFunction();
+
+Product.insertMany([
+    {
+        name:'alex',
+        price:25,
+        category:'fruit',
+        id:Math.ceil(Math.random()*500)
+    },
+    {
+        name:'Organic',
+        price:2,
+        category:'vega',
+        id:Math.ceil(Math.random()*500)
+    },
+    {
+        name:'Orange',
+        price:6,
+        category:'fruit',
+        id:Math.ceil(Math.random()*500)
+    },
+    {
+        name:'cholate',
+        price:8,
+        category:'fruit',
+        id:Math.ceil(Math.random()*500)
+    },
+    {
+        name:'clery',
+        price:25,
+        category:'fruit',
+        id:Math.ceil(Math.random()*500)
+    }
+
+]).then(()=>console.log('insert succed'))
+  .catch((err)=>console.log(err))
